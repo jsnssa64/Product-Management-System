0314ead2e67250b695339df05a15a1194deb80f8
Jason Sousa <jason.sousa@derivco.co.uk>
2022-10-09 13:31:48 +0100

    Base DbUp project

---
+++ ProductManagement.Data/Program.cs
@@ -0,0 +1,33 @@
+﻿using DbUp;
+using System.Reflection;
+
+var connectionString =
+        args.FirstOrDefault()
+        ?? "Server=(local)\\SqlExpress; Database=MyApp; Trusted_connection=true";
+
+EnsureDatabase.For.SqlDatabase(connectionString);
+
+var upgrader =
+    DeployChanges.To
+        .SqlDatabase(connectionString)
+        .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+        .LogToConsole()
+        .Build();
+
+var result = upgrader.PerformUpgrade();
+
+if (!result.Successful)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(result.Error);
+    Console.ResetColor();
+#if DEBUG
+    Console.ReadLine();
+#endif
+    return -1;
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Success!");
+Console.ResetColor();
+return 0;
