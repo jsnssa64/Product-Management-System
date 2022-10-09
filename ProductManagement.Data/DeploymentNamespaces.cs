ec03029a7db6e1734951d26554814f06cbb0a1de
Jason Sousa <jason.sousa@derivco.co.uk>
2022-10-09 22:22:45 +0100

    Partial Completion of multi-environment deployments with pre and post deploy

---
+++ ProductManagement.Data/DeploymentNamespaces.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagement.Data
+{
+    public class DeploymentNamespaces
+    {
+        public string PreDeploymentNamespace { get; set; }
+        public string PostDeploymentNamespace { get; set; }
+    }
+}
--- ProductManagement.Data/Program.cs
+++ ProductManagement.Data/Program.cs
@@ -1,33 +1,112 @@
 ﻿using DbUp;
+using Microsoft.Extensions.Configuration;
+using ProductManagement.Data;
 using System.Reflection;
 
-var connectionString =
-        args.FirstOrDefault()
-        ?? "Server=(local)\\SqlExpress; Database=MyApp; Trusted_connection=true";
+//  Production, Staging, Development
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
-EnsureDatabase.For.SqlDatabase(connectionString);
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile($"appsettings.json", false, true)
+    .AddJsonFile($"appsettings.{environment}.json", true, true)
+    .AddEnvironmentVariables()
+    .Build();
 
-var upgrader =
-    DeployChanges.To
-        .SqlDatabase(connectionString)
-        .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
-        .LogToConsole()
-        .Build();
+var connectionString = configuration.GetConnectionString("connection");
 
-var result = upgrader.PerformUpgrade();
+//EnsureDatabase.For.SqlDatabase(connectionString);
+Console.WriteLine("\nIMPORTANT: Please ensure your scripts are EMBEDDED in the executable.");
 
-if (!result.Successful)
+//  Base 
+var baseNamespace = Assembly.GetExecutingAssembly().GetName().Name;
+//  Environment
+var environmentsNamespace = baseNamespace + ".Environments";
+
+var deploymentNamespaces = configuration.GetSection(nameof(DeploymentNamespaces))
+                                    .Get<DeploymentNamespaces>();
+
+var Env = configuration["Environment"];
+
+//  Variables
+var variables = configuration.GetSection("variables")
+                            .GetChildren()
+                            .ToDictionary(x => x.Key, x => x.Value);
+foreach (var pair in variables)
+{
+    Console.WriteLine($"{pair.Key} = \"{pair.Value}\"");
+}
+
+//  Base Predeployments
+string preDeploymentScriptsPath = baseNamespace + ".PreDeployment";
+RunMigrations(connectionString, preDeploymentScriptsPath, variables);
+
+//  Environment Pre-Deployments (Optional)
+if (deploymentNamespaces != null)
+{
+    RunMigrations(connectionString, deploymentNamespaces.PreDeploymentNamespace, variables);
+}
+
+//  Base Migrations
+var migrationScriptsPath = baseNamespace + ".Migrations";
+RunMigrations(connectionString, migrationScriptsPath, variables);
+
+//  Base Post Deployments
+string postDeploymentScriptsPath = baseNamespace + ".PostDeployment";
+RunMigrations(connectionString, postDeploymentScriptsPath, variables);
+
+//  Optional Environment Post Deployments
+if (deploymentNamespaces != null)
+{
+    RunMigrations(connectionString, deploymentNamespaces.PreDeploymentNamespace, variables);
+}
+
+//await host.RunAsync();
+return 0;
+
+static int RunMigrations(string connectionString, 
+    string @namespace,
+    Dictionary<string, string> variables)
+{
+    Console.WriteLine($"Executing scripts in {@namespace}");
+    var upgrader =
+        DeployChanges.To
+            .SqlDatabase(connectionString)
+            .WithVariables(variables)
+            .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly(), file =>
+            {
+                return file
+                        .ToLower()
+                        .StartsWith(@namespace.ToLower());
+            })
+            .LogToConsole()
+            .Build();
+
+    var result = upgrader.PerformUpgrade();
+
+    if (!result.Successful)
+    {
+        ShowError(result.Error);
+        return -1;
+    }
+
+    ShowSuccess();
+    return 0;
+}
+
+static void ShowSuccess()
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Success!");
+    Console.ResetColor();
+}
+
+static void ShowError(Exception exception)
 {
     Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine(result.Error);
+    Console.WriteLine(exception);
     Console.ResetColor();
 #if DEBUG
     Console.ReadLine();
 #endif
-    return -1;
 }
-
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine("Success!");
-Console.ResetColor();
-return 0;
